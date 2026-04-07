d2d549d4638982dc79403ff73fa5c11cb6c4090d
MysticMichelle <163708012+MysticMichelle@users.noreply.github.com>
2026-04-07 06:59:19 +0000

    Elementos de la Ruta Dashboard y rutas anidadas

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -11,9 +11,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.className} antialiased`}>
+        Esto es parte del layout
         <h1>Este titulo</h1>
         <div style={{ border: '1px solid #000' }}>
           {children}
+          <footer className="py-10 flex justify-center items-center">
+            Hecho con ❤️ por la gente de Vercel
+          </footer>
         </div>
       </body>
     </html>
